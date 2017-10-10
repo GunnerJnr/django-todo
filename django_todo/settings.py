a4062a23cd70c1cf1cee7b6a5c92bdb0eb323a08
David Gunner Jnr <gunnerjnr@live.co.uk>
2017-10-10 13:32:34 +0100

    settings modified

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -136,10 +136,10 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8080',
-    'http://127.0.0.1:8080',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
+    'http://localhost:8080',
+    'http://127.0.0.1:8080',
 )
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
